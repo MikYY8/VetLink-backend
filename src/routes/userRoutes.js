696cfc76234f4c03fa4b5b4ef4b31010c94fe9d9
Micaela Ybarra <mica.yaz03@gmail.com>
2026-01-06 16:18:16 -0300

    registers, logins and token refresh working just fine

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { registerController, loginController, loginVetController, renovateTokenController } from "../controllers/userController.js"
-import { registerValidation, loginValidation, loginVetValidation } from "../validations/userValidation.js";
+import { registerController, registerVetController, loginController, loginVetController, renovateTokenController } from "../controllers/userController.js"
+import { registerValidation, loginValidation } from "../validations/userValidation.js";
 import { validationMiddleware } from "../middlewares/validationMiddleware.js";
+// import { authVetMiddleware } from "../middlewares/authVetMiddleware.js" 
+//  authVetMiddleware NO es para el LOGIN!!!! ES PARA RUTAS DEL VET
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerValidation, validationMiddleware, registerController);
+userRouter.post("/vet/register", registerValidation, validationMiddleware, registerVetController);
 userRouter.post("/login", loginValidation, validationMiddleware, loginController);
-userRouter.post("/vet/login", loginVetValidation, validationMiddleware, loginVetController);
+userRouter.post("/vet/login", loginValidation, validationMiddleware, loginVetController);
 userRouter.post("/token", renovateTokenController);
 
 export default userRouter
