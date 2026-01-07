9f92f60a7b783aef22cd860d343da37a9fb9dcad
Micaela Ybarra <mica.yaz03@gmail.com>
2026-01-07 17:42:19 -0300

    some fixes and comments for clarity

--- src/middlewares/authRolesMiddleware.js
+++ src/middlewares/authRolesMiddleware.js
@@ -1,8 +1,13 @@
-export const authRoles = (roles = []) =>{ 
+// Auturiza a un usuario a acceder a cierta informacion
+// dependiendo de su rol
+
+export const authRolesMiddleware = (roles = []) =>{ 
     return (req,res,next) =>{
+        // Si el usuario no tiene sesión iniciada
         if(!req.user){
             return res.status(401).json({message: "Debe iniciar sesión para acceder"})
         }
+        // Si el usuario no tiene el rol necesario
         if (!roles.includes(req.user.role)){
             return res.status(401).json({message: "Acceso no autorizado"})
         }
