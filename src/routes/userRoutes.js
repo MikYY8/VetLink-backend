3c9798e3ca5a363e728fafda0f2e17481e73e601
Micaela Ybarra <mica.yaz03@gmail.com>
2026-01-13 19:08:36 -0300

    Admins and secretaries can modify and delete users and vets

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { registerController, registerVetController, loginController, loginVetController, renovateTokenController } from "../controllers/userController.js"
+import { registerController, registerVetController, 
+        loginController, loginVetController, 
+        renovateTokenController, getAllUsersController,
+        getAllVetsController, 
+        updateUserController, deleteUserController,
+        updateVetController, deleteVetController
+         } from "../controllers/userController.js"
 import { registerValidation, loginValidation } from "../validations/userValidation.js";
 import { validationMiddleware } from "../middlewares/validationMiddleware.js";
+import { authRolesMiddleware } from "../middlewares/authRolesMiddleware.js" // (["OWNER", "ADMIN", "SECRETARY"])
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
@@ -21,5 +29,20 @@ userRouter.post("/vet/login", loginValidation, validationMiddleware, loginVetCon
 
 // Renovar token 
 userRouter.post("/token", renovateTokenController);
+
+  // *** funciones EXCLUSIVAMENTE administrativas! ***
+
+// listar todos los usuarios
+userRouter.get("/allusers", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), getAllUsersController)
+// listar todos los vets
+userRouter.get("/allvets", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), getAllVetsController);
+// editar un usuario
+userRouter.put("/edit-user/:ownerId", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), updateUserController)
+// eliminar un usuario
+userRouter.delete("/delete-user/:ownerId", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), deleteUserController)
+// editar un vet
+userRouter.put("/edit-vet/:vetId", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), updateVetController)
+// eliminar un vet
+userRouter.delete("/delete-vet/:vetId", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), deleteVetController)
 
 export default userRouter
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,14 +2,14 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js";
 import Usuario from "../models/userModel.js"
-import Vet from "../models/vetModel.js";
+import Veterinario from "../models/vetModel.js";
 
 export class userService {
     async registerUser (firstName, lastName, email, password, role) {
         const existingUser = await Usuario.findOne({ email });
         if(existingUser){
             throw new Error("E-mail ya registrado");
-        }
+        };
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await Usuario.create({firstName, lastName, email, password : hashedPassword, role});
@@ -17,25 +17,25 @@ export class userService {
     };
 
     async registerVet (firstName, lastName, email, password, licenseNumber, specialty, workSchedule, acceptsConsultations){
-        const existingVet = await Vet.findOne({ email });
+        const existingVet = await Veterinario.findOne({ email });
         if(existingVet){
             throw new Error("E-mail ya registrado");
-        }
+        };
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newVet = await Vet.create({ firstName, lastName, email, password : hashedPassword, licenseNumber, specialty, workSchedule, acceptsConsultations })
+        const newVet = await Veterinario.create({ firstName, lastName, email, password : hashedPassword, licenseNumber, specialty, workSchedule, acceptsConsultations })
         return newVet;
-    }
+    };
 
     async loginUser (email, password) {
         try{
             const userExists = await Usuario.findOne({ email });
             if (!userExists) {
                 throw new Error("Email o contraseña incorrectos");
-            }
+            };
             const passwordValid = await bcrypt.compare(password, userExists.password);
             if (!passwordValid) {
                 throw new Error("Email o contraseña incorrectos");
-            }
+            };
             const accesstoken = generateAccessToken({
                 id: userExists._id,
                 email: userExists.email,
@@ -50,20 +50,20 @@ export class userService {
 
         }catch(error) {
             throw new Error(error);
-        }
+        };
     };
 
     async loginVet (email, password) {
         try{
-            const vetExists = await Vet.findOne({ email });
+            const vetExists = await Veterinario.findOne({ email });
             if (!vetExists) {
                 throw new Error("Email o contraseña incorrectos");
-            }
+            };
 
             const passwordValid = await bcrypt.compare(password, vetExists.password);
             if (!passwordValid) {
                 throw new Error("Email o contraseña incorrectos");
-            }
+            };
 
             const accesstoken = generateAccessToken({
                 id: vetExists._id,
@@ -79,7 +79,7 @@ export class userService {
 
         }catch(error) {
             throw new Error(error);
-        }
+        };
     };
 
     async renovateAccessToken(refreshtoken) {
@@ -87,9 +87,35 @@ export class userService {
         const user = await Usuario.findById(payload.id);
         if (!user) {
             throw new Error("No se encontró el usuario");
-        }
+        };
         const accesstoken = generateAccessToken({id: user._id, email: user.email, role: user.role,});
         return accesstoken
-    }
+    };  
 
-}
+        // EDITAR DATOS DE UN USUARIO
+    async updateUser(ownerId, updateData) {
+        const user = await Usuario.findById(ownerId);
+
+        if (!user) {
+            throw new Error("Usuario no encontrado");
+        };
+
+        Object.assign(user, updateData);
+        await user.save();
+        return user;
+    };
+
+        // EDITAR DATOS DE UN VETERINARIO
+    async updateVet(vetId, updateData) {
+        const vet = await Veterinario.findById(vetId);
+
+        if (!vet) {
+            throw new Error("Veterinario no encontrado");
+        };
+
+        Object.assign(vet, updateData);
+        await vet.save();
+        return vet;
+    };
+
+};
