6c1716508ceebe1d389b4a3b9df2ca325e443bce
Micaela Ybarra <mica.yaz03@gmail.com>
2026-01-15 22:47:06 -0300

    routes look better now

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -2,16 +2,19 @@ import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js"; // token valido 
 import { authRolesMiddleware } from "../middlewares/authRolesMiddleware.js" // (["OWNER", "ADMIN", "SECRETARY"])
 
+import { getAvailableAppointmentsController, getVetAppointmentsController,
+        getAllAppointmentsController,
+        } from "../controllers/appointmentController.js";
 
 const appointmentRouter = express.Router();
 
-// Turnos disponibles (PARA OWNERS)
-appointmentRouter.get("/available");
+// Turnos disponibles (PARA OWNERS / SECRETARIES / ADMINS)
+appointmentRouter.get("/available", authMiddleware, authRolesMiddleware(["OWNER", "SECRETARY", "ADMIN"]), getAvailableAppointmentsController);
 
 // Turnos programados (PARA VETS)
-appointmentRouter.get("/scheduled");
+appointmentRouter.get("/scheduled", authMiddleware, authRolesMiddleware(["VET"]), getVetAppointmentsController);
 
 // Todos los turnos, habidos y por haber (PARA SECRETARIES / ADMINS)
-appointmentRouter.get("/scheduled");
+appointmentRouter.get("/all-appointments", authMiddleware, authRolesMiddleware(["ADMIN", "SECRETARY"]), getAllAppointmentsController);
 
-export default ownerRouter
+export default appointmentRouter
